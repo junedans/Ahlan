5f1a4602bd44ba7d6cb89201032c6a62fcea50fa
sa-ok <sa-ok>
2018-04-01 21:42:45 -0700

    CardMonth updates

--- src/components/CardMonth/CardMonth.ts
+++ src/components/CardMonth/CardMonth.ts
@@ -7,7 +7,44 @@ import * as Content from "../../model/appContent";
 })
 export class CardMonth {
   card: Content.CardMonth;
+  mDates = new Array<[string, string, string]>();
+
+  static sLang1 = "en-US";
+  static sLang2 = "ar";
+  static sMonthIndexes = {
+    January:    "01",
+    February:   "02",
+    March:      "03",
+    April:      "04",
+    May:        "05",
+    June:       "06",
+    July:       "07",
+    August:     "08",
+    Sepetember: "09",
+    October:    "10",
+    November:   "11",
+    December:   "12"
+  };
 
   constructor() {
+  }
+
+  public ngOnInit() {      
+    this.card.dates.forEach(date => {
+      var key = "";
+      if (date instanceof Content.SpecificDate)
+      {
+        key = (date as Content.SpecificDate).dayOfMonth[CardMonth.sLang1] + "";
+        if (key.length == 1) {
+          key = "0" + key;
+        }
+        key =  CardMonth.sMonthIndexes[this.card.title[CardMonth.sLang1]] + "/" + key;
+      }
+      else {
+        var gdate = date as Content.GenericDate;
+        key = gdate.dayOfWeek[CardMonth.sLang1] + " of week " + gdate.weekOfMonth[CardMonth.sLang1];
+      }
+      this.mDates.push([key, date.title[CardMonth.sLang1], date.title[CardMonth.sLang2]]);
+    });
   }
 }
